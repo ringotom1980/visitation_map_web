038f7f5d5f38a610c026233496a27b29ada94d0a
ringotom1980 <ringo_tom2007@yahoo.com.tw>
2025-12-13 17:57:19 +0800

    改標記視窗大小

--- Public/partials/head.php
+++ Public/partials/head.php
@@ -19,8 +19,7 @@ if (!isset($pageCss) || !is_array($pageCss)) {
 <head>
   <meta charset="UTF-8">
   <title><?= htmlspecialchars((string)$pageTitle, ENT_QUOTES, 'UTF-8') ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
 
   <link rel="icon" type="image/png" href="<?= asset_url('assets/img/logo.png') ?>">
 
